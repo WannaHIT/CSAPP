5c628b14b3f997f08d78b9f04d804d1bcc020df7
litao <591179801@qq.com>
2025-07-05 23:56:19 +0800

    lowbit,

--- test/code/x.c
+++ test/code/x.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+unsigned LowBit(unsigned x)
+{
+	unsigned a = 0;
+	// a = 1000
+	// a = x & (-x);
+	a = x & ((~x) + 1);
+	return a;
+}
+
 int main(void)
 {
 	int a = 2147483647;
 	int b = 1;
-
 	
-	printf("%d\n", (~a)+1);
-	printf("%d\n", (a)+1);
-	printf("%d\n", ~a);
+	printf("%d\n", LowBit(3));
+	
+	// printf("%d\n", (~a)+1);
+	// printf("%d\n", (a)+1);
+	// printf("%d\n", ~a);
 
 	return 0;
 }
